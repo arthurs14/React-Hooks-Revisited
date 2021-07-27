6b7ef548964c9ee6ae9a7483926bc97d0abb90b4
arthurs14 <arthurcs426@gmail.com>
2021-07-27 14:03:43 -0700

    small fix to get favorites page to show favorited products; ;

--- src/containers/Favorites.js
+++ src/containers/Favorites.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React  from 'react';
 
 import FavoriteItem from '../components/Favorites/FavoriteItem';
+import { useProducts } from '../context/products-context';
 import './Products.css';
 
 const Favorites = props => {
-  const favoriteProducts = useSelector(state =>
-    state.shop.products.filter(p => p.isFavorite)
-  );
+  const { products } = useProducts();
+
+  const favoriteProducts = products.filter(product => product.isFavorite);
+  
   let content = <p className="placeholder">Got no favorites yet!</p>;
   if (favoriteProducts.length > 0) {
     content = (
