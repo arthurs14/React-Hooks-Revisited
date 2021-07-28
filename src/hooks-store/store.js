0832a2aa69c883b9b1256fdadb762b1a3522663a
arthurs14 <arthurcs426@gmail.com>
2021-07-28 00:23:22 -0700

    documenting how a custom hook is made to mimic redux to a degree

--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 
+// share in entire project
+// defined outside hook to share data
 let globalState = {};
 let listeners = [];
 let actions = {};
@@ -11,21 +13,27 @@ export const useStore = () => {
     const newState = actions[actionIdentifier](globalState, payload);
     globalState = { ...globalState, ...newState };
 
+    // setState calls
     for (const listener of listeners) {
       listener(globalState);
     }
   };
 
   useEffect(() => {
+    // register one component when rendered
     listeners.push(setState);
-     return () => {
-       listeners = listeners.filter(li => li !== setState);
-     };
+
+    // unregister component when destroyed
+    return () => {
+      listeners = listeners.filter(li => li !== setState);
+    };
   }, [setState]);
 
   return [globalState, dispatch];
 };
 
+// way to initialize our store and call multiple times
+// create concrete store slices similar to redux
 export const initStore = (userActions, initialState) => {
   if (initialState) {
     globalState = { ...globalState, ...initialState };
